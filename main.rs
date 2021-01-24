8bf3d981787835f2b9a5b5edf579387ba33daa3a
Jake Cooper <jake.elijah.cooper@gmail.com>
2021-01-24 07:35:46 +0000

    Initial commit

---
+++ main.rs
@@ -0,0 +1,11 @@
+
+#[macro_use] extern crate rocket;
+
+#[get("/hello/<name>/<age>")]
+fn hello(name: String, age: u8) -> String {
+    format!("Hello, {} year old named {}!", age, name)
+}
+
+fn main() {
+    rocket::ignite().mount("/", routes![hello]).launch();
+}
